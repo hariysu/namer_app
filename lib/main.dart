d69217858d23a69959345c6c6c62499fe8d880cf
Abdurahman AYDIN <hariysu@gmail.com>
2023-03-15 15:34:06 +0300

    Prepares widget folder and moves big_card here2

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
-import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import 'providers/my_app_state.dart';
+import 'widgets/big_card.dart';
 
 void main() {
   runApp(MyApp());
@@ -67,35 +67,6 @@ class MyHomePage extends StatelessWidget {
               ],
             ),
           ],
-        ),
-      ),
-    );
-  }
-}
-
-class BigCard extends StatelessWidget {
-  const BigCard({
-    Key? key,
-    required this.pair,
-  }) : super(key: key);
-
-  final WordPair pair;
-
-  @override
-  Widget build(BuildContext context) {
-    var theme = Theme.of(context);
-    var style = theme.textTheme.displayMedium!.copyWith(
-      color: theme.colorScheme.onPrimary,
-    );
-
-    return Card(
-      color: theme.colorScheme.primary,
-      child: Padding(
-        padding: const EdgeInsets.all(20),
-        child: Text(
-          pair.asLowerCase,
-          style: style,
-          semanticsLabel: "${pair.first} ${pair.second}",
         ),
       ),
     );
